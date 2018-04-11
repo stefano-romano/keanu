f84d727373e021b817fc3b913ef5213e81a96c81
dmculley <daveculley@improbable.io>
2018-04-11 15:26:07 +0100

    Simplification

--- keanu-examples/appleton/src/main/kotlin/AppleTree.kt
+++ keanu-examples/appleton/src/main/kotlin/AppleTree.kt
@@ -3,10 +3,9 @@ import io.improbable.keanu.kotlin.DoubleOperators
 /**
  * Let's start with number of apples being continuous then discretise later
  */
-class AppleTree<DOUBLE : DoubleOperators<DOUBLE>>(val xLocation: DOUBLE, val yLocation: DOUBLE,
-                                                  initialAppleCount: DOUBLE, val appleGrowthRate: DOUBLE) : IAgent {
+class AppleTree<DOUBLE: DoubleOperators<DOUBLE>>(val xLocation: Double, val yLocation: Double, var appleCount: DOUBLE,
+                                                 val appleGrowthRate: Double) : IAgent {
 
-    var appleCount = initialAppleCount
 
     override fun step() {
         appleCount += appleGrowthRate
--- keanu-examples/appleton/src/main/kotlin/ScrumperTypes.kt
+++ keanu-examples/appleton/src/main/kotlin/ScrumperTypes.kt
@@ -1,31 +1,6 @@
-import io.improbable.keanu.kotlin.ArithmeticDouble
-import io.improbable.keanu.kotlin.DoubleOperators
-import io.improbable.keanu.vertices.dbl.DoubleVertex
-import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex
+enum class ScrumperTypes (val speed: Double, visionRange: Double) {
 
-class ScrumperTypes<DOUBLE : DoubleOperators<DOUBLE>> (val speed: DOUBLE, visionRange: DOUBLE) {
-
-    var martianSpeed = 0.38
-    var martianVisionRange = 7.0
-    var venusianSpeed = 0.9
-    var venusianVisionRange = 3.0
-    var earthlingSpeed = 1.0
-    var earthlingVisionRange = 10.0
-
-    lateinit var MARTIAN: ScrumperTypes<DOUBLE>
-    lateinit var VENUSIAN: ScrumperTypes<DOUBLE>
-    lateinit var EARTHLING: ScrumperTypes<DOUBLE>
-
-
-    init {
-        if (speed is ArithmeticDouble) {
-            MARTIAN = ScrumperTypes(ArithmeticDouble(martianSpeed) as DOUBLE, ArithmeticDouble(martianVisionRange) as DOUBLE)
-            VENUSIAN = ScrumperTypes(ArithmeticDouble(venusianSpeed) as DOUBLE, ArithmeticDouble(venusianVisionRange) as DOUBLE)
-            EARTHLING = ScrumperTypes(ArithmeticDouble(earthlingSpeed) as DOUBLE, ArithmeticDouble(earthlingVisionRange) as DOUBLE)
-        } else if (speed is DoubleVertex) {
-            MARTIAN = ScrumperTypes(ConstantDoubleVertex(martianSpeed) as DOUBLE, ConstantDoubleVertex(martianVisionRange) as DOUBLE)
-            VENUSIAN = ScrumperTypes(ConstantDoubleVertex(venusianSpeed) as DOUBLE, ConstantDoubleVertex(venusianVisionRange) as DOUBLE)
-            EARTHLING = ScrumperTypes(ConstantDoubleVertex(earthlingSpeed) as DOUBLE, ConstantDoubleVertex(earthlingVisionRange) as DOUBLE)
-        }
-    }
+    MARTIAN(0.38, 7.0),
+    VENUSIAN(0.9, 3.0),
+    EARTHLING(1.0, 10.0)
 }
