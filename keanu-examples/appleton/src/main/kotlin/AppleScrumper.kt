a8c6d9e2ebb9ab6b3b4942f942a24cf12298430d
dmculley <daveculley@improbable.io>
2018-04-11 15:38:48 +0100

    Pre-pulling Jon's additions

--- keanu-examples/appleton/src/main/kotlin/AppleScrumper.kt
+++ keanu-examples/appleton/src/main/kotlin/AppleScrumper.kt
@@ -1,9 +1,24 @@
 import io.improbable.keanu.kotlin.DoubleOperators
 
-class AppleScrumper<DOUBLE : DoubleOperators<DOUBLE>> (var xLocation: DOUBLE, var yLocation: DOUBLE, hunger: DOUBLE,
-                                                       val agressionLevel: DOUBLE) : IAgent {
+class AppleScrumper<DOUBLE : DoubleOperators<DOUBLE>> (val world: World, var xLocation: DOUBLE, var yLocation: DOUBLE,
+                                                       hunger: DOUBLE, val agressionLevel: DOUBLE,
+                                                       val species: ScrumperTypes) : IAgent {
 
     override fun step() {
+
+        // Check what's around
+        var surroundings = world.lookAround(species.visionRange)
+
+        // If there's some things there, determine priority
+
+
+            // If not in range to action that priority
+
+                // Move toward
+
+            // Else action the priority (attack competitor or pick apples)
+
+        // Else continue in previous direction of travel
 
     }
 
--- keanu-examples/appleton/src/main/kotlin/ScrumperTypes.kt
+++ keanu-examples/appleton/src/main/kotlin/ScrumperTypes.kt
@@ -1,4 +1,4 @@
-enum class ScrumperTypes (val speed: Double, visionRange: Double) {
+enum class ScrumperTypes (val speed: Double, val visionRange: Double) {
 
     MARTIAN(0.38, 7.0),
     VENUSIAN(0.9, 3.0),
