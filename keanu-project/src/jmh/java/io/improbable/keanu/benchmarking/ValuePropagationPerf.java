cf91c816286f43f57149374f4cea3baf1213c6fc
Caleb <caleb@improbable.io>
2018-08-10 09:54:35 +0100

    simple link test

--- keanu-project/src/jmh/java/io/improbable/keanu/benchmarking/ValuePropagationPerf.java
+++ keanu-project/src/jmh/java/io/improbable/keanu/benchmarking/ValuePropagationPerf.java
@@ -1,9 +1,13 @@
 package io.improbable.keanu.benchmarking;
 
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -15,10 +19,38 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class ValuePropagationPerf {
 
-    @Benchmark
-    public void methodOne() {
+    @State(Scope.Benchmark)
+    public static class TestGraph {
+
+        volatile DoubleVertex start;
+
+        public TestGraph(){
+            addLinks(100000);
+        }
+
+        private DoubleVertex addLinks(int links) {
+
+            start = new GaussianVertex(0, 1);
+            DoubleVertex end = start;
+
+            for (int i = 0; i < links; i++) {
+                DoubleVertex left = end.sigmoid();
+                DoubleVertex right = end.exp();
+                end = left.times(right);
+            }
+
+            start.setAndCascade(1.0);
+
+            return end;
+        }
 
     }
+
+    @Benchmark
+    public void methodOne(TestGraph testGraph) {
+        testGraph.start.setAndCascade(testGraph.start.getValue(0) + 1);
+    }
+
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
