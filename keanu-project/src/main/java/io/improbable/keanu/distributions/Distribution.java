e521a79ec7d85e6225fb0e89e5825d11247a2048
Joanne Hong <joannehong@improbable.io>
2018-08-22 15:32:23 +0100

    Implement computeKLDivergence methods to discrete distributions

--- keanu-project/src/main/java/io/improbable/keanu/distributions/Distribution.java
+++ keanu-project/src/main/java/io/improbable/keanu/distributions/Distribution.java
@@ -3,8 +3,12 @@ package io.improbable.keanu.distributions;
 import io.improbable.keanu.tensor.Tensor;
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
 import io.improbable.keanu.vertices.dbl.KeanuRandom;
+import org.apache.commons.lang3.NotImplementedException;
 
-public interface Distribution<T extends Tensor> {
+public interface Distribution<T extends Tensor> extends BaseDistribution {
     T sample(int[] shape, KeanuRandom random);
     DoubleTensor logProb(T x);
+    default Support<T> getSupport() {
+        throw new NotImplementedException("getSupport is not implemented for this distribution");
+    }
 }
